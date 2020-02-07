186d675ea70b040347b3fc61c71c87b856ea8fb9
ramazan199 <ramazansuleymanli@gmail.com>
2020-02-07 12:40:56 +0200

    categ

--- src/components/Collapse/Feed.vue
+++ src/components/Collapse/Feed.vue
@@ -5,12 +5,7 @@
       <fg-input placeholder="Search" v-model="inputVal"></fg-input>
     </div>
     <p>{{ inputVal }}</p>
-    <div class="container button-container row">
-      <n-button type="primary" class="btn btn-primary btn-round btn-sm">everything</n-button>
-      <n-button type="info" class="btn btn-neutral btn-round btn-sm">winter sports</n-button>
-      <n-button type="info" class="btn btn-neutral btn-round btn-sm">food</n-button>
-      <n-button type="info" class="btn btn-neutral btn-round btn-sm">skydiving</n-button>
-    </div>
+   
     <div class="container text-center">
       <div class="row justify-content-md-center">
         <div class="col-md-12 col-lg-8">
@@ -19,19 +14,55 @@
       </div>
     </div>
 
-    <collapse>
-      <div v-for="item in items" v-bind:key="item.title">
-        <collapse-item class="container" v-bind:title="item.title" v-bind:name="item.name">
-          <div>{{item.content}}</div>
+    <tabs type="primary">
+      <tab-pane label="Wintwe sports">
+        <collapse>
+          <div v-for="item in items" v-bind:key="item.title">
+            <collapse-item class="container" v-bind:title="item.title" v-bind:name="item.name">
+              <div>{{item.content}}</div>
 
-          <div class="button-container">
-            <a href="#/profile" class="btn btn-primary btn-round btn-lg">view profile</a>
-            <a href="#/profile" class="btn btn-primary btn-round btn-lg">message</a>
+              <div class="button-container">
+                <a href="#/profile" class="btn btn-primary btn-round btn-lg">view profile</a>
+                <a href="#/profile" class="btn btn-primary btn-round btn-lg">message</a>
+              </div>
+            </collapse-item>
+            <br />
           </div>
-        </collapse-item>
-        <br />
-      </div>
-    </collapse>
+        </collapse>
+      </tab-pane>
+
+      <tab-pane label="Skydiving">
+        <collapse>
+          <div v-for="item in items2" v-bind:key="item.title">
+            <collapse-item class="container" v-bind:title="item.title" v-bind:name="item.name">
+              <div>{{item.content}}</div>
+
+              <div class="button-container">
+                <a href="#/profile" class="btn btn-primary btn-round btn-lg">view profile</a>
+                <a href="#/profile" class="btn btn-primary btn-round btn-lg">message</a>
+              </div>
+            </collapse-item>
+            <br />
+          </div>
+        </collapse>
+      </tab-pane>
+
+      <tab-pane label="Food">
+        <collapse>
+          <div v-for="item in items3" v-bind:key="item.title">
+            <collapse-item class="container" v-bind:title="item.title" v-bind:name="item.name">
+              <div>{{item.content}}</div>
+
+              <div class="button-container">
+                <a href="#/profile" class="btn btn-primary btn-round btn-lg">view profile</a>
+                <a href="#/profile" class="btn btn-primary btn-round btn-lg">message</a>
+              </div>
+            </collapse-item>
+            <br />
+          </div>
+        </collapse>
+      </tab-pane>
+    </tabs>
 
     <div class="section">
       <div class="container text-center">
@@ -51,13 +82,16 @@ import Collapse from "./Collapse.vue";
 import CollapseItem from "./CollapseItem.vue";
 import FgInput from "../Inputs/formGroupInput";
 import Button from "../Button";
+import {Tabs, TabPane} from '@/components';
 
 export default {
   components: {
     Collapse,
     CollapseItem,
     FgInput,
-    Button
+    Button,
+    Tabs,
+    TabPane
   },
 
   name: "feed",
@@ -72,7 +106,76 @@ export default {
         user0: {
           name: "Ryan Scheinder",
           title: "go snowboarding",
-          content: "I want to try out snowboarding before I'm 30. None of my friends are open to learning and I don't really want to get an instructor."
+          content:
+            "I want to try out snowboarding before I'm 30. None of my friends are open to learning and I don't really want to get an instructor."
+        },
+        user1: {
+          name: "Jack Daniel",
+          title: "ride a bull",
+          content: "aa"
+        },
+        user2: { name: "Tom Brady", title: "go skydiving", content: "aa" },
+        user3: {
+          name: "Tasneem Haque",
+          title: "try Georgian food",
+          content: "aa"
+        },
+        user4: {
+          name: "Marina Sarapuu",
+          title: "try Georgian food",
+          content: "aa"
+        },
+        user5: {
+          name: "Samuel Sanchez",
+          title: "try Georgian food",
+          content: "aa"
+        },
+        user6: {
+          name: "Pavel Valge",
+          title: "try Georgian food",
+          content: "aa"
+        }
+      },
+      items2: {
+        user0: {
+          name: "Karim Scheinder",
+          title: "ride a bull",
+          content:
+            "I want to try out snowboarding before I'm 30. None of my friends are open to learning and I don't really want to get an instructor."
+        },
+        user1: {
+          name: "Anna Danni",
+          title: "ride a bull",
+          content: "aa"
+        },
+        user2: { name: "Tom Brady", title: "go skydiving", content: "aa" },
+        user3: {
+          name: "Tasneem Haque",
+          title: "try Georgian food",
+          content: "aa"
+        },
+        user4: {
+          name: "Marina Sarapuu",
+          title: "try Georgian food",
+          content: "aa"
+        },
+        user5: {
+          name: "Samuel Sanchez",
+          title: "try Georgian food",
+          content: "aa"
+        },
+        user6: {
+          name: "Pavel Valge",
+          title: "try Georgian food",
+          content: "aa"
+        }
+      },
+      items3: {
+        user0: {
+          name: "Mark Rhian",
+          title: "try Mexican food",
+          content:
+            "I want to try out snowboarding before I'm 30. None of my friends are open to learning and I don't really want to get an instructor."
         },
         user1: {
           name: "Jack Daniel",
@@ -101,6 +204,7 @@ export default {
           content: "aa"
         }
       }
+
     };
   }
 };
